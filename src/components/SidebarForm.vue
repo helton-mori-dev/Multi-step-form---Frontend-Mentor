a76e489f3a1223a99fe4f02e43e0fe6783861140
Helton Mori <heltonmori@gmail.com>
2023-11-05 16:47:12 -0300

    Sidebar incompleto

---
+++ src/components/SidebarForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="sidebar"></section>
+</template>
+<script>
+export default {
+  name: "SidebarForm",
+  data() {
+    return {
+      steps: {
+        "step 1": "Your info",
+        "step 2": "Select plan",
+        "step 3": "Add-ons",
+        "step 4": "Summary",
+      },
+    };
+  },
+};
+</script>
